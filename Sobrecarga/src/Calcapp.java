8aa567f174d2b5c4b45f6acedc5129fb220bf5f1
Servando Reyes <andres92898@gmail.com>
2017-06-05 21:03:27 -0300

    init
    Nuevos cambios en la clase Calcapp

--- Sobrecarga/src/Calcapp.java
+++ Sobrecarga/src/Calcapp.java
@@ -8,6 +8,18 @@ public class Calcapp {
 		System.out.println( "Suma" );
 		System.out.println( calc.suma("20.3", "33.44", "2.0", "77.88", "41.32") );
 		
+		System.out.println( "Suma" );
+		System.out.println( calc.suma( 2, 2 ) );
+		
+		System.out.println( "Suma" );
+		System.out.println( calc.suma( 2, 2, 2 ) );
+		
+		System.out.println( "Suma" );
+		System.out.println( calc.suma( 2.2, 2.3 ) );
+		
+		System.out.println( "Suma" );
+		System.out.println( calc.suma( 2.2, 2.3, 2.4 ) );
+		
 	}
 
 }
